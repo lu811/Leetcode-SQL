98fc746ecfa78c5b4890852b6f9c7fc74365c90d
lu811 <70782468+lu811@users.noreply.github.com>
2020-11-13 13:33:51 -0500

    Update 181. Employees Earning More Than Their Managers.sql

--- 181. Employees Earning More Than Their Managers.sql
+++ 181. Employees Earning More Than Their Managers.sql
@@ -22,3 +22,9 @@ Employee AS a,
 Employee AS b
 WHERE a.ManagerId = b.Id
 AND a.Salary > b.Salary
+
+Mine: 
+select E.name as Employee from Employee E
+join Employee as M
+on E.ManagerId = M.Id
+where E.Salary > M.Salary;
