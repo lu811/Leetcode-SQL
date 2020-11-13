819198119ed55b1127917504508bc422f42df2a9
lu811 <70782468+lu811@users.noreply.github.com>
2020-11-13 13:46:08 -0500

    Update 182. Duplicate Emails.sql

--- 182. Duplicate Emails.sql
+++ 182. Duplicate Emails.sql
@@ -33,3 +33,17 @@ Person as a,
 Person as b
 WHERE a.Email = b.Email
 AND a.Id > b.Id
+
+select Email from
+(
+  select Email, count(Email) as num
+  from Person
+  group by Email
+) as statistic
+where num > 1
+;
+
+select Email
+from Person
+group by Email
+having count(Email) > 1;
