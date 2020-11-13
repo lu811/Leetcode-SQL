b98f6e648ca1b419d6e18c0baf0e2d4f595e8ff0
lu811 <70782468+lu811@users.noreply.github.com>
2020-11-13 11:12:25 -0500

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -35,3 +35,12 @@ SELECT
         ORDER BY Salary DESC
         LIMIT 1 OFFSET 1) AS SecondHighestSalary
 ;
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
